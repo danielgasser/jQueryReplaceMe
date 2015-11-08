59e159d6dc4afee628b7c5ac2b99ec63bbe6e72f
Daniel Gasser <info@pc-shooter.ch>
2015-11-08 15:47:52 +0100

    excluded match bug

--- jQueryReplaceMe.js
+++ jQueryReplaceMe.js
@@ -15,14 +15,14 @@
         }, options);
         $(this).contents().each(function (index, node) {
             var text,
-                excludedTag;
+                excludedTags;
             if(node.innerHTML !== undefined) {
                 text = node.innerHTML;
             } else {
                 text = node.nodeValue
             }
-            excludedTag = (node.localName === null) ? '' : node.localName;
-            if (text.match(new RegExp(settings.textToReplace, 'g')) && node.nodeType === 3 && !excludedTag.match(new RegExp(settings.excludedTags.join('|')))) {
+            excludedTags = (node.localName === null || node.localName === undefined) ? '' : node.localName;
+            if (text.match(new RegExp(settings.textToReplace, 'g')) && node.nodeType === 3 && !excludedTags.match(new RegExp(settings.excludedTags.join('|')))) {
                 if(settings.globally) {
                     $(node).replaceWith(text.replace(new RegExp(settings.textToReplace, 'g'), settings.replaceWithText));
                 } else {
