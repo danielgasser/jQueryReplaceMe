51f39d29bcc45d8d0ea234740df650352c395be3
Daniel Gasser <software@toesslab.ch>
2018-03-05 12:16:20 +0100

    Eliminated jQuery's each, replaced with for

--- jQueryReplaceMe.js
+++ jQueryReplaceMe.js
@@ -4,23 +4,26 @@
 (function ($) {
     "use strict";
     $.fn.replaceMe = function (options) {
-        var settings = $.extend({
-            textToReplace: 'any_text',
-            replaceWithText: '<span>othertext</span>',
-            globally: true,
-            excludedParentTags: [
-                'HEAD',
-                'TITLE',
-                'LINK',
-                'SCRIPT',
-                'STYLE',
-                'footer'
-            ], excludedTags: [
-                'img',
+        var contents = $(this).contents(),
+            contentIndex,
+            node,
+            index,
+            settings = $.extend({
+                textToReplace: 'any_text',
+                replaceWithText: '<span>othertext</span>',
+                globally: true,
+                excludedParentTags: [
+                    'HEAD',
+                    'TITLE',
+                    'LINK',
+                    'SCRIPT',
+                    'STYLE',
+                    'footer'
+                ], excludedTags: [
+                    'img',
 
-            ], excludedSelectors: [
-            ]
-        }, options),
+                ], excludedSelectors: []
+            }, options),
             checkExCludedSelectors = function (el) {
                 var _el = el,
                     index,
@@ -45,35 +48,26 @@
         if (settings.excludedTags.length === 0) {
             settings.excludedTags.push('title')
         }
-        $(this).contents().each(function (index, node) {
+        for (contentIndex = 0; contentIndex < contents.length; contentIndex += 1) {
+            node = contents[contentIndex];
+            index = contentIndex;
             var text,
                 excludedTags;
-            console.log(node)
-            switch (node) {
-                case node.innerHTML !== undefined:
-                    text = node.innerHTML;
-                    break;
-                case node.wholeText !== undefined:
-                    text = node.wholeText;
-                    break;
-                case node.textContent !== undefined:
-                    text = node.textContent;
-                    break;
-            }
+            text = $(node).text();
             text = (text === undefined) ? '' : text;
             excludedTags = (node.localName === null || node.localName === undefined) ? node.nodeName : node.localName;
             checkExCludedSelectors(excludedTags);
             console.log(text)
-            if (text.match(new RegExp(settings.textToReplace, 'g')) && node.nodeType === 3 && !excludedTags.match(new RegExp(settings.excludedTags.join('|'))) && $.inArray(node.parentElement.nodeName, settings.excludedParentTags) === -1) {
-                if(settings.globally) {
+            if (text.match(new RegExp(settings.textToReplace, 'g')) && !excludedTags.match(new RegExp(settings.excludedTags.join('|'))) && $.inArray(node.parentElement.nodeName, settings.excludedParentTags) === -1) {
+                if (settings.globally) {
                     $(node).replaceWith(text.replaceAll(settings.textToReplace, settings.replaceWithText));
                 } else {
                     $(node).replaceWith(text.replace(settings.textToReplace, settings.replaceWithText));
                 }
             }
-        });
+        }
     }
-    String.prototype.replaceAll = function(search, replace){
+    String.prototype.replaceAll = function (search, replace) {
         var string = this,
             index;
         while ((index = string.indexOf(search)) !== -1) {
