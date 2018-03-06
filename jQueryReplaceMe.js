d9a48d738eebcef29d3577117ca2d34325b5918c
Daniel Gasser <software@toesslab.ch>
2018-03-06 07:10:06 +0100

    minified (uglified) version added

--- jQueryReplaceMe.js
+++ jQueryReplaceMe.js
@@ -1,39 +1,37 @@
-/**
- * Created by https://toesslab.ch on 9/20/15.
- */
 (function ($) {
     "use strict";
     $.fn.replaceMe = function (options) {
         var contents = $(this).contents(),
             contentIndex,
+            text,
+            excludedTags,
             node,
             index,
             settings = $.extend({
-                textToReplace: 'any_text',
-                replaceWithText: '<span>othertext</span>',
+                textToReplace: "any text",
+                replaceWithText: "<span>othertext</span>",
                 globally: true,
                 excludedParentTags: [
-                    'HEAD',
-                    'TITLE',
-                    'LINK',
-                    'SCRIPT',
-                    'STYLE',
-                    'footer'
+                    "HEAD",
+                    "TITLE",
+                    "LINK",
+                    "SCRIPT",
+                    "STYLE"
                 ],
                 excludedTags: [
-                    'img'
+                    "img"
                 ],
                 excludedSelectors: []
             }, options),
             checkExCludedSelectors = function (el) {
                 var _el = el,
-                    index,
+                    excludeIndex,
                     childIndex,
                     entry,
                     childEntry,
                     element;
-                for (index = 0; index < settings.excludedSelectors.length; index += 1) {
-                    entry = settings.excludedSelectors[index];
+                for (excludeIndex = 0; excludeIndex < settings.excludedSelectors.length; excludeIndex += 1) {
+                    entry = settings.excludedSelectors[excludeIndex];
                     if (($(_el).hasClass(entry) || $(_el).is(entry)) && $.inArray(entry, settings.excludedTags) === -1) {
                         settings.excludedTags.push(_el);
                         element = $(_el).children();
@@ -47,19 +45,16 @@
                 }
             };
         if (settings.excludedTags.length === 0) {
-            settings.excludedTags.push('title')
+            settings.excludedTags.push("title");
         }
         for (contentIndex = 0; contentIndex < contents.length; contentIndex += 1) {
             node = contents[contentIndex];
             index = contentIndex;
-            var text,
-                excludedTags;
             text = $(node).text();
-            text = (text === undefined) ? '' : text;
+            text = (text === undefined) ? "" : text;
             excludedTags = (node.localName === null || node.localName === undefined) ? node.nodeName : node.localName;
             checkExCludedSelectors(excludedTags);
-            console.log(text)
-            if (text.match(new RegExp(settings.textToReplace, 'g')) && !excludedTags.match(new RegExp(settings.excludedTags.join('|'))) && $.inArray(node.parentElement.nodeName, settings.excludedParentTags) === -1) {
+            if (text.match(new RegExp(settings.textToReplace, "g")) && !excludedTags.match(new RegExp(settings.excludedTags.join("|"))) && $.inArray(node.parentElement.nodeName, settings.excludedParentTags) === -1) {
                 if (settings.globally) {
                     $(node).replaceWith(text.replaceAll(settings.textToReplace, settings.replaceWithText));
                 } else {
@@ -67,7 +62,7 @@
                 }
             }
         }
-    }
+    };
     String.prototype.replaceAll = function (search, replace) {
         var string = this,
             index;
@@ -75,5 +70,5 @@
             string = string.replace(search, replace);
         }
         return string;
-    }
+    };
 }(jQuery));
